d4456f5deecfa22d49f73988dd98e53d937abd23
Raphael Phillipe <raphaelphillips14@gmail.com>
2024-01-25 20:06:14 -0300

    Final repository

---
+++ README.md
@@ -0,0 +1,44 @@
+<div align="center">
+<h1 align="center"> Sprint 1 Challenge ONE - Turma 06</h1>
+<img align="center" src="![Logo challenge](assets/img/image.png)">
+<p>O programa ONE - Oracle next education é um programa da Oracle em parceria com alura, de educação e empregabilidade com objetivo social de capacitar pessoas em tecnologia e conectá-las com o mercado de trabalho por meio de empresas parceiras.</p>
+</div>
+<div align="center">
+  <a href="#sobre-o-desafio">Sobre o desafio</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#Site">Site</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</div>
+<br>
+<img src="![./Readme-IMGS/mockup_1.png](assets/img/image2.png)" alt="Project Mockup"/>
+<img src="![./Readme-IMGS/mockup_2.png](assets/img/image3.png)" alt="Project Mockup"/>
+
+
+<h1 id="sobre-o-desafio"> Sobre o desafio</h1>
+
+Desenvolver uma aplicação que criptografa e descriptografa textos.
+
+### As "chaves" de criptografia são:
+
+>A letra "e" é convertida para "enter"
+>A letra "i" é convertida para "imes"
+>A letra "a" é convertida para "ai"
+>A letra "o" é convertida para "ober"
+>A letra "u" é convertida para "ufat"
+
+Requisitos:
+>- Deve funcionar apenas com letras minúsculas
+>- Não devem ser utilizados letras com acentos nem caracteres especiais
+>- Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
+>Por exemplo:
+```
+"gato" => "gaitober"
+gaitober" => "gato"
+```
+>- Um botão que copie o texto criptografado/descriptografado para a área de transferência - ou seja, que tenha a mesma funcionalidade do ctrl+C ou da opção "copiar" do menu dos aplicativos.
+>A página deve ter campos para inserção do texto a ser criptografado ou descriptografado, e a pessoa usuária deve poder escolher entre as duas opções
+O resultado deve ser exibido na tela.
+
+<h2 id="Site">Site finalizado</h2>
+<p></p>
+<br>
+<img src="![Alt text](assets/img/video.gif)" alt="gif-project">
+
---
+++ app.js
@@ -0,0 +1,105 @@
+const myModal = document.getElementById('myModal');
+const modalText = document.getElementById('modal-text');
+
+function Remover(palavra){
+   return palavra.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^a-zA-Z ]/g, ''); 
+}
+
+function CriptografarFunction(){
+    const originalInput = document.getElementById('original'); 
+    const palavraOriginal = Remover(originalInput.value.trim().toLocaleLowerCase()); 
+    const resultado = document.getElementById('resultado'); 
+
+    if(palavraOriginal !== ''){
+        const palavraCriptografada = Criptografar(palavraOriginal);
+        resultado.innerText =`${palavraCriptografada}`;
+    }
+    else{
+        modalText.textContent = 'Digite um texto primeiro';
+        showOrHideModal();
+    }
+}
+
+function DescriptografarFunction(){
+    const originalInput = document.getElementById('original'); 
+    const palavraCriptografada = Remover(originalInput.value.trim().toLowerCase());
+    const resultado = document.getElementById('resultado'); 
+
+    if (palavraCriptografada !== ''){
+        const palavraOriginal = Descriptografar(palavraCriptografada);
+        resultado.innerText=`${palavraOriginal}`;
+    }
+    else {
+        modalText.textContent = 'Digite um texto primeiro';
+        showOrHideModal();
+    }
+}
+
+function Criptografar(palavra){
+    return palavra
+    .replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/a/g, 'ai')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat');
+}
+
+function Descriptografar(palavraCriptografada){
+    return palavraCriptografada
+    .replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ai/g, 'a')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u');
+}
+
+function LixeiraFunction(){
+    document.getElementById('original').value = '';
+    document.getElementById('resultado').innerText = '';
+    document.getElementById('card-message').style.display='none';
+    document.getElementById('no-content').style.display='block'; 
+}
+
+async function CopiarFunction(){
+
+        if(resultado.innerText ===""){
+        modalText.textContent = 'Selecione uma das opções abaixo antes de copiar';
+        showOrHideModal();
+        }
+        else{
+        await navigator.clipboard.writeText(resultado.innerText);
+        modalText.textContent = 'Copiado para área de transferência!';
+        showOrHideModal();
+        }
+}
+
+function TutorialFunction(){
+    modalText.textContent = 'Digite uma palavra no campo acima e veja seu resultado criptografado ou descriptografado, você pode copiar o resultado também!';
+    showOrHideModal();
+}
+
+function handleInput() {
+    var originalInput = document.getElementById("original").value;
+    var noContentDiv = document.getElementById("no-content");
+    var cardMessageDiv = document.getElementById("card-message");
+
+    if (originalInput.trim() === "") {
+      noContentDiv.style.display = "block";
+      cardMessageDiv.style.display = "none";
+    } else {
+      noContentDiv.style.display = "none";
+      cardMessageDiv.style.display = "block";
+    }
+  }
+  
+  document.getElementById("original").addEventListener("input", handleInput, function() {
+  document.getElementById("no-content").style.display = "none";});
+
+  handleInput();
+
+  const closeModalBtn = document.getElementById('close-btn');
+  closeModalBtn.addEventListener('click', showOrHideModal);
+  
+  function showOrHideModal(){
+      myModal.classList.toggle('hidden');
+  }
